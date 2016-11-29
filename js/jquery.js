10b0bcdc42e41ec39d122a4baa9ae7481b1b8a22
Talon Bragg <pvpgamecrash@gmail.com>
2016-11-29 07:24:56 -0800

    Update jquery.js

--- js/jquery.js
+++ js/jquery.js
@@ -1,9 +1,9 @@
-<!-- Number One -->
-$(document).ready(function() {
-  $('#title1Click').click(function() {
-    $('#slidetitle').animate({
-      textAlign:'center',
-      fontSize:'3em'
+$(document).ready(function () {
+    $('#button').click(function () {
+        var toAdd = $('input[name=checkListItem]').val();
+        $('.list').append( $('<div class="item">' + toAdd + '</div         >'));
     });
-  });
+    $(document).on('click', '.item', function(){
+        $(this).remove();    
+    });
 });
